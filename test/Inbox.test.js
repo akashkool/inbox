32222809407173aedc43618bb821750f072caa39
akashkool <akashkool96@gmail.com>
2023-08-11 00:25:44 +0530

    print accounts from ganache in test file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,26 +7,16 @@ const { Web3 } = require('web3');
 // we may use other provider like RinkeBy, Ethereum ,Polygon etc  with Web3 class.
 const web3 = new Web3(ganache.provider());
 
-class Car{
-    park(){
-        return 'stopped';
-    }
-    drive(){
-        return 'vroom';
-    }
-}
-
-let car;
-
 beforeEach(()=>{
-    car = new Car();
+    // get a list of all account & use one of the account to deploy smart contract.
+    // eth stands for ethereum.
+    web3.eth.getAccounts().then(fetchedAccounts=>{
+        console.log(fetchedAccounts)
+    })
 })
 describe('Car',()=>{
-    it('can park',()=>{
-        assert.equal(car.park(),'stopped');
-    })
-    it('can drive',()=>{
-        assert.equal(car.drive(),'vroom');
+    it('deploys a contract',()=>{
+
     })
 })
 
